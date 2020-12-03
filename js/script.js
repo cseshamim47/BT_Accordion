376c5327cc7c16b4b66d3f85ad217e5c15f99a22
Md Shamim Ahmed <73248611+cseshamim47@users.noreply.github.com>
2020-12-03 17:46:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var alertList = document.querySelectorAll('.alert')
+alertList.forEach(function (alert) {
+  new bootstrap.Alert(alert)
+})
+
+
+var myAlert = document.getElementById('myAlert')
+myAlert.addEventListener('closed.bs.alert', function () {
+  // do something, for instance, explicitly move focus to the most appropriate element,
+  // so it doesn't get lost/reset to the start of the page
+  // document.getElementById('...').focus()
+})
